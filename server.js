778fb4dfd204dd3a0fdb26a4cc4a3ef7d4c92713
edpaynter <edisonxpaynter@gmail.com>
2019-06-16 10:59:11 -0400

    added user and pw to datbase

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/myScrape";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user1:password1@ds137827.mlab.com:37827/heroku_zjttg08q";
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
